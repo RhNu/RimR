c419cfab3343b1c058a9f76239e9633733e2e85b
Rhynia <65334131+RhNu@users.noreply.github.com>
2026-06-27 14:34:14 +0800

    chore(vite): update build config

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,11 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-
   clearScreen: false,
   server: {
     port: 5173,
@@ -22,13 +20,23 @@ export default defineConfig({
     host: host || false,
     hmr: host
       ? {
-          protocol: 'ws',
-          host,
-          port: 5174,
-        }
+        protocol: 'ws',
+        host,
+        port: 5174,
+      }
       : undefined,
     watch: {
       ignored: ['**/src-tauri/**', '**/crates/**'],
     },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+    chunkSizeWarningLimit: 1000,
   },
 });
